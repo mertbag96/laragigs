9ff24b9fd833608500ec80d43c162432936318c6
Mert Bağ <bagmert1996@gmail.com>
2024-09-07 21:42:12 +0300

    Models, Migrations, Factories & Seeder

---
+++ app/Models/Listing.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'title',
+        'tags',
+        'company',
+        'location',
+        'email',
+        'website',
+        'description',
+    ];
+}
